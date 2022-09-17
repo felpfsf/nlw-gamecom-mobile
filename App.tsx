980fc1d6971414175745359c40ac14ca3572ee31
felpfsf <feferreira.sf@gmail.com>
2022-09-17 16:53:35 -0300

    feat: push notifications

--- App.tsx
+++ App.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef } from 'react'
 import { StatusBar } from 'react-native'
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications'
+
 import {
   useFonts,
   Inter_400Regular,
@@ -7,12 +11,45 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
+import { Routes } from './src/routes'
 import { Background } from './src/components/Background/Background'
 import { Loading } from './src/components/Loading/Loading'
 
-import { Routes } from './src/routes'
+import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, [])
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener(notification => {
+        console.log(notification)
+      })
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response)
+      })
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        )
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        )
+      }
+    }
+  }, [])
+
   const [isFontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
